98fa7dcc4cef3864393dcde9367e0e9c6546fff7
Esau <esaurenteria.isc@gmail.com>
2022-11-16 12:56:49 -0600

    Agregando alertas para resaltar campos en blanco

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
 const inputEmail = document.querySelector('#email');
 const inputAsunto = document.querySelector('#asunto');
 const inputMensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('#formulario');
 
 //Asignar eventos
 
@@ -13,10 +14,33 @@ inputMensaje.addEventListener('blur', validar);
 
 function validar(e){
     if(e.target.value.trim() === ''){
-        console.log('Esta vacio');
-    }else{
-        console.log('Hay algo');
+        mostrarAlerta(`El Campo ${e.target.id} es obligatorio`, e.target.parentElement);
+        return;
     }
+    limpiarAlerta(e.target.parentElement);
+}
+
+
+function mostrarAlerta(mensaje, referencia){
+
+  limpiarAlerta(referencia);
+  
+    //Generar alerta en HTML
+    const error = document.createElement('P');
+    error.textContent = mensaje;
+    error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+
+    //Inyectar el error al formulario
+    referencia.appendChild(error);
+}
+
+function limpiarAlerta(referencia){
+    const alerta = referencia.querySelector('.bg-red-600')
+
+    if(alerta){
+        alerta.remove();
+    }
+
 }
 
 });
