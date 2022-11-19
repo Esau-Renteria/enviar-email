98493ec3fa8d5dd21e072874644889ea3d6b20b0
Esau <esaurenteria.isc@gmail.com>
2022-11-19 11:09:35 -0600

    Agregando funcion para activar o desactivar el boton de enviar

--- js/app.js
+++ js/app.js
@@ -1,29 +1,47 @@
 document.addEventListener('DOMContentLoaded', function(){
 //Seleccionar los elementos de la interfaz
 
+
+const email={
+    email: '',
+    asunto: '',
+    mensaje: ''
+}
+
 const inputEmail = document.querySelector('#email');
 const inputAsunto = document.querySelector('#asunto');
 const inputMensaje = document.querySelector('#mensaje');
 const formulario = document.querySelector('#formulario');
+const btnSubmit = document.querySelector('#formulario button[type="submit"]')
 
 //Asignar eventos
 
-inputEmail.addEventListener('blur', validar);
-inputAsunto.addEventListener('blur', validar);
-inputMensaje.addEventListener('blur', validar);
+inputEmail.addEventListener('input', validar);
+inputAsunto.addEventListener('input', validar);
+inputMensaje.addEventListener('input', validar);
 
 function validar(e){
     if(e.target.value.trim() === ''){
         mostrarAlerta(`El Campo ${e.target.id} es obligatorio`, e.target.parentElement);
+        email[e.target.name]='';
+        comprobarEmail();
         return;
     }
 
     if(e.target.id === 'email' && !validarEmail(e.target.value)){
         mostrarAlerta('El email no es valido', e.target.parentElement);
+        email[e.target.name]='';
+        comprobarEmail();
         return;
     };
 
     limpiarAlerta(e.target.parentElement);
+
+
+    email[e.target.name]= e.target.value.trim().toLowerCase();
+    
+    //Comprobar el objeto de email
+    comprobarEmail();
 }
 
 
@@ -57,5 +75,13 @@ function validarEmail(email){
     return resultado;
  
 }
-
+function comprobarEmail(){
+    if(Object.values(email).includes('')){
+        btnSubmit.classList.add('opacity-50');
+    btnSubmit.disabled = true;
+}else{
+    btnSubmit.classList.remove('opacity-50');
+    btnSubmit.disabled = false;
+}
+}
 });
